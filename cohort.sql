b3f34c49f82570be8f3f4841bce69bac8b11f181
Bram Gadeyne <gadeynebram@gmail.com>
2014-09-06 18:28:16 +0200

    cohort is now extended to patients who have lactulose more then once a day.

--- cohort.sql
+++ cohort.sql
@@ -1,9 +1,43 @@
+drop table if exists tmp_chirr_cohort;
 create table tmp_chirr_cohort as
 select distinct id.icustay_id
 from mimic2v26.icustay_detail id
-join mimic2v26.icd9 cirrh on cirrh.hadm_id = id.hadm_id and cirrh.code in ('571.2','571.5','571.6')
-join mimic2v26.poe_order io on io.icustay_id = id.icustay_id and lower(io.medication) like '%lactulose%'
-and icustay_seq = 1
+join mimic2v26.poe_order io on 
+	io.icustay_id = id.icustay_id and 
+	lower(io.medication) like '%lactulose%' and
+	io.frequency in (
+	'5X/D',
+	'BID',
+	'Q1-2H:PRN',
+	'Q1H',
+	'Q1H:PRN',
+	'Q2-3H',
+	'Q2-3H:PRN',
+	'Q2-4H',
+	'Q2-4H:PRN',
+	'Q2H',
+	'Q2H:PRN',
+	'Q3-6H:PRN',
+	'Q3H',
+	'Q3H:PRN',
+	'Q4-6H',
+	'Q4H',
+	'Q4H:PRN',
+	'Q6-8H',
+	'Q6-8H:PRN',
+	'Q6H',
+	'Q6H:PRN',
+	'Q8H',
+	'Q 8H',
+	'Q8H:PRN',
+	'QAM',
+	'QD',
+	'QID',
+	'QID:PRN',
+	'TID',
+	'TID:PRN'
+	)
+where icustay_seq = 1
 and id.icustay_first_service IN ('MICU')
 and id.icustay_los >= (72*60)
-and lower(id.icustay_age_group) = 'adult'
+and lower(id.icustay_age_group) = 'adult';
